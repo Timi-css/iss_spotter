ecc65ebbf5c8955b56ce49733383ccac072a056f
Timi Alabi <alabitimi65@gmail.com>
2022-06-11 08:31:10 +0000

    changes made

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { fetchMyIP } = require("./iss");
 const { fetchCoordsByIP } = require("./iss");
+const { fetchISSFlyOverTimes } = require("./iss");
+const { nextISSTimesForMyLocation } = require("./iss");
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -18,4 +20,38 @@ fetchCoordsByIP("162.245.144.188", (error, coordinates) => {
   }
 
   console.log("It worked! Returned coordinates:", coordinates);
+});
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log("It worked! Returned flyover times:", passTimes);
+});
+
+const { nextISSTimesForMyLocation } = require("./iss");
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  console.log(passTimes);
+});
+const printPassTimes = function (passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
 });
